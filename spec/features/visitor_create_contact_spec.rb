7ec69bcf193337878139a582ab6c860d02a25b5b
ABDNdev <igor@abidin.ru>
2020-01-09 20:26:03 +0300

    Update tests(allows a guest to create contact

--- spec/features/visitor_create_contact_spec.rb
+++ spec/features/visitor_create_contact_spec.rb
@@ -1,10 +1,21 @@
 require "rails_helper"
 
 feature "Contact Creation" do 
-  scenario "allows acees to contact page" do
+  scenario "allows accees to contact page" do
     visit '/contacts'
 
     expect(page).to have_content I18n.t('contacts.contact_us')
-  end   
+  end 
+
+  scenario "allows a guest to create contact" do
+    visit '/contacts'
+
+    fill_in :contact_email, :with => 'user@example.com'
+    fill_in :contact_message, :with => 'something'
+
+    click_button 'Send message'
+
+    expect(page).to have_content 'Thanks!'
+  end  
 end
 
