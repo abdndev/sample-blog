d754af9da2f5ca97311e67fa7baa28268daf98c8
ABDNdev <igor@abidin.ru>
2020-01-11 18:16:14 +0300

    rails_helper.rb modification

--- spec/features/visitor_creates_account_spec.rb
+++ spec/features/visitor_creates_account_spec.rb
@@ -1,6 +1,6 @@
 require "rails_helper"
 
-feature "Account Creation" do 
+feature "Account Creation" do
   scenario "allows guest to create account" do
     sign_up
     expect(page).to have_content I18n.t('devise.registrations.signed_up')
